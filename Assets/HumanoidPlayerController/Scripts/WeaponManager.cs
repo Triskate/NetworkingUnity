0b5a844d3ad7755a0064e8e28499bfab5b5d3258
Triskate <albertislatorres@gmail.com>
2025-01-09 20:47:52 +0100

    WeaponSystem

---
+++ Assets/HumanoidPlayerController/Scripts/WeaponManager.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField] InputActionReference changeWeapon;
+    [SerializeField] InputActionReference shoot;
+    FireWeapon[] fireWeapons;
+    int currentWeapon = -1;
+
+    private void Awake()
+    {
+        fireWeapons = GetComponentsInChildren<FireWeapon>();
+    }
+
+    private void OnEnable()
+    {
+        changeWeapon.action.Enable();
+        changeWeapon.action.performed += OnChangeWeapon;
+
+        shoot.action.Enable();
+        shoot.action.performed += OnShoot;
+    }
+
+
+    private void OnDisable()
+    {
+        changeWeapon.action.Disable();
+        changeWeapon.action.performed -= OnChangeWeapon;
+
+        shoot.action.Disable();
+        shoot.action.performed -= OnShoot;
+    }
+
+
+    private void OnChangeWeapon(InputAction.CallbackContext context)
+    {
+        Vector2 delta = context.ReadValue<Vector2>();
+
+        if (currentWeapon != -1) { fireWeapons[currentWeapon].NotifyDeselected(); }
+
+        if (delta.y > 0) 
+        {
+            currentWeapon++;
+            if (currentWeapon >= fireWeapons.Length) { currentWeapon = -1; }
+        }
+        else
+        {
+            currentWeapon--;
+            if (currentWeapon < -1) { currentWeapon = fireWeapons.Length - 1; }
+        }
+
+        if (currentWeapon != -1) { fireWeapons[currentWeapon].NotifySelected(); }
+    }
+
+    private void OnShoot(InputAction.CallbackContext context)
+    {
+        if(currentWeapon != -1)
+        {
+            fireWeapons[currentWeapon].Shot();
+        }
+    }
+}
--- Assets/WeaponSystem/Scripts/FireWeapon.cs
+++ Assets/WeaponSystem/Scripts/FireWeapon.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class FireWeapon : MonoBehaviour
@@ -20,11 +21,26 @@ public class FireWeapon : MonoBehaviour
     {
         barrels = GetComponentsInChildren<Barrel>();
     }
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
     public void Shot()
     {
         foreach (Barrel b in barrels)
         {
             b.Shot();
         }
+    }
+
+    internal void NotifySelected()
+    {
+        gameObject.SetActive(true);
+    }
+
+    internal void NotifyDeselected()
+    {
+        gameObject.SetActive(false);
     }
 }
